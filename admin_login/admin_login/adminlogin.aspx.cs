3813c71d260f27e899fe6361844674f5b1636991
swiper0708 <vominhthanh07082004@gmail.com>
2024-11-02 02:03:22 +0700

    admin_login

---
+++ admin_login/admin_login/adminlogin.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace admin_login
+{
+    public partial class adminlogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Buttonlogin_Click(object sender, EventArgs e)
+        {
+            string Username = Usernamelg.Text.Trim();
+            String Pass = Passlg.Text.Trim();
+
+            if (AuthenticateUser(Username, Pass))
+            {
+                //Dang nhap thanh cong
+                lblMessage.Text = "Đăng nhập thành công";
+                lblMessage.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                //Dang nhap that bai
+                lblMessage.Text = "Email hoặc mật khẩu không đúng!";
+            }
+        }
+
+        private bool AuthenticateUser(String Username, String Pass)
+        {
+            Lopketnoi kn = new Lopketnoi();
+            string query = $"SELECT * FROM KHACHHANG WHERE TENDANGNHAP = N'{Username}' AND MATKHAU ='{Pass}'";
+            DataTable dt = kn.LayDuLieu(query);
+            return dt!= null && dt.Rows.Count > 0;
+        }
+    }
+}
